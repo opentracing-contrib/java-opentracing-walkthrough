a11c85cefe27bfdaba51cc4f0e93da0d61d59ff4
Ben Sigelman <bhs@lightstep.com>
2017-05-06 20:43:01 -0700

    Rename dronutz as MicroDonuts

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,2 +1,2 @@
 set -e
-java -cp dronutz/target/api-1.0-SNAPSHOT.jar -Dorg.eclipse.jetty.util.log.classrg.eclipse.jetty.util.log.StdErrLog com.otsample.api.App
+java -cp microdonuts/target/api-1.0-SNAPSHOT.jar -Dorg.eclipse.jetty.util.log.classrg.eclipse.jetty.util.log.StdErrLog com.otsample.api.App
