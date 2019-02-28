5657d8c7f19813a927d00a7d05d8d62c26db8f90
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2019-02-28 01:53:18 +0100

    Initial changes for testing the SpecialAgent.

--- microdonuts/src/main/java/com/otsample/api/App.java
+++ microdonuts/src/main/java/com/otsample/api/App.java
@@ -1,7 +1,5 @@
 package com.otsample.api;
 
-import brave.Tracing;
-import brave.opentracing.BraveTracer;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.net.MalformedURLException;
@@ -12,15 +10,6 @@ import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-
-import io.opentracing.Tracer;
-import io.opentracing.util.GlobalTracer;
-import com.lightstep.tracer.jre.JRETracer;
-import com.lightstep.tracer.shared.Options;
-import zipkin.reporter.AsyncReporter;
-import zipkin.reporter.Reporter;
-import zipkin.reporter.Sender;
-import zipkin.reporter.okhttp3.OkHttpSender;
 
 public class App
 {
@@ -61,47 +50,5 @@ public class App
         Properties config = new Properties();
         config.load(fs);
         return config;
-    }
-
-    static boolean configureGlobalTracer(Properties config, String componentName)
-        throws MalformedURLException
-    {
-        String tracerName = config.getProperty("tracer");
-        if ("jaeger".equals(tracerName)) {
-            GlobalTracer.register(
-                new com.uber.jaeger.Configuration(
-                    componentName,
-                    new com.uber.jaeger.Configuration.SamplerConfiguration("const", 1),
-                    new com.uber.jaeger.Configuration.ReporterConfiguration(
-                        true,  // logSpans
-                        config.getProperty("jaeger.reporter_host"),
-                        Integer.decode(config.getProperty("jaeger.reporter_port")),
-                        1000,   // flush interval in milliseconds
-                        10000)  // max buffered Spans
-                ).getTracer());
-        } else if ("zipkin".equals(tracerName)){
-            Sender sender = OkHttpSender.create(
-                "http://" +
-                    config.getProperty("zipkin.reporter_host") + ":" +
-                    config.getProperty("zipkin.reporter_port") + "/api/v1/spans");
-            Reporter reporter = AsyncReporter.builder(sender).build();
-            GlobalTracer.register(BraveTracer.create(Tracing.newBuilder()
-                .localServiceName(componentName)
-                .reporter(reporter)
-                .build()));
-        } else if ("lightstep".equals(tracerName)) {
-            Options opts = new Options.OptionsBuilder()
-                .withAccessToken(config.getProperty("lightstep.access_token"))
-                .withCollectorHost(config.getProperty("lightstep.collector_host"))
-                .withCollectorPort(Integer.decode(config.getProperty("lightstep.collector_port")))
-                .withComponentName(componentName)
-                .build();
-            Tracer tracer = new JRETracer(opts);
-            GlobalTracer.register(tracer);
-        } else {
-            return false;
-        }
-
-        return true;
     }
 }
